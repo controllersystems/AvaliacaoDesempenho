6b19437774b146cdbe65410ac09fd3d3126a8cc9
controllersystems <rodrigo.cruz@outlook.com>
2014-09-11 15:02:16 -0300

    Add Documentação

---
+++ Documentação/1 - avaliacao_desempenho_create.sql
@@ -0,0 +1,119 @@
+USE [master]
+GO
+
+/****** Object:  Database [avaliacao_desempenho]    Script Date: 19/08/2014 16:26:08 ******/
+
+IF (EXISTS (SELECT name FROM master.dbo.sysdatabases WHERE ('[' + name + ']' = 'avaliacao_desempenho'
+OR name = 'avaliacao_desempenho')))
+DROP DATABASE [avaliacao_desempenho]
+GO
+
+/****** Object:  Database [avaliacao_desempenho]    Script Date: 19/08/2014 16:26:08 ******/
+CREATE DATABASE [avaliacao_desempenho]
+ CONTAINMENT = NONE
+ ON  PRIMARY 
+( NAME = N'avaliacao_desempenho', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL11.MSSQLSERVER\MSSQL\DATA\avaliacao_desempenho.mdf' , SIZE = 5120KB , MAXSIZE = UNLIMITED, FILEGROWTH = 1024KB )
+ LOG ON 
+( NAME = N'avaliacao_desempenho_log', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL11.MSSQLSERVER\MSSQL\DATA\avaliacao_desempenho_log.ldf' , SIZE = 2048KB , MAXSIZE = 2048GB , FILEGROWTH = 10%)
+GO
+
+ALTER DATABASE [avaliacao_desempenho] SET COMPATIBILITY_LEVEL = 120
+GO
+
+IF (1 = FULLTEXTSERVICEPROPERTY('IsFullTextInstalled'))
+begin
+EXEC [avaliacao_desempenho].[dbo].[sp_fulltext_database] @action = 'enable'
+end
+GO
+
+ALTER DATABASE [avaliacao_desempenho] SET ANSI_NULL_DEFAULT OFF 
+GO
+
+ALTER DATABASE [avaliacao_desempenho] SET ANSI_NULLS OFF 
+GO
+
+ALTER DATABASE [avaliacao_desempenho] SET ANSI_PADDING OFF 
+GO
+
+ALTER DATABASE [avaliacao_desempenho] SET ANSI_WARNINGS OFF 
+GO
+
+ALTER DATABASE [avaliacao_desempenho] SET ARITHABORT OFF 
+GO
+
+ALTER DATABASE [avaliacao_desempenho] SET AUTO_CLOSE OFF 
+GO
+
+ALTER DATABASE [avaliacao_desempenho] SET AUTO_SHRINK OFF 
+GO
+
+ALTER DATABASE [avaliacao_desempenho] SET AUTO_UPDATE_STATISTICS ON 
+GO
+
+ALTER DATABASE [avaliacao_desempenho] SET CURSOR_CLOSE_ON_COMMIT OFF 
+GO
+
+ALTER DATABASE [avaliacao_desempenho] SET CURSOR_DEFAULT  GLOBAL 
+GO
+
+ALTER DATABASE [avaliacao_desempenho] SET CONCAT_NULL_YIELDS_NULL OFF 
+GO
+
+ALTER DATABASE [avaliacao_desempenho] SET NUMERIC_ROUNDABORT OFF 
+GO
+
+ALTER DATABASE [avaliacao_desempenho] SET QUOTED_IDENTIFIER OFF 
+GO
+
+ALTER DATABASE [avaliacao_desempenho] SET RECURSIVE_TRIGGERS OFF 
+GO
+
+ALTER DATABASE [avaliacao_desempenho] SET  DISABLE_BROKER 
+GO
+
+ALTER DATABASE [avaliacao_desempenho] SET AUTO_UPDATE_STATISTICS_ASYNC OFF 
+GO
+
+ALTER DATABASE [avaliacao_desempenho] SET DATE_CORRELATION_OPTIMIZATION OFF 
+GO
+
+ALTER DATABASE [avaliacao_desempenho] SET TRUSTWORTHY OFF 
+GO
+
+ALTER DATABASE [avaliacao_desempenho] SET ALLOW_SNAPSHOT_ISOLATION OFF 
+GO
+
+ALTER DATABASE [avaliacao_desempenho] SET PARAMETERIZATION SIMPLE 
+GO
+
+ALTER DATABASE [avaliacao_desempenho] SET READ_COMMITTED_SNAPSHOT OFF 
+GO
+
+ALTER DATABASE [avaliacao_desempenho] SET HONOR_BROKER_PRIORITY OFF 
+GO
+
+ALTER DATABASE [avaliacao_desempenho] SET RECOVERY FULL 
+GO
+
+ALTER DATABASE [avaliacao_desempenho] SET  MULTI_USER 
+GO
+
+ALTER DATABASE [avaliacao_desempenho] SET PAGE_VERIFY CHECKSUM  
+GO
+
+ALTER DATABASE [avaliacao_desempenho] SET DB_CHAINING OFF 
+GO
+
+ALTER DATABASE [avaliacao_desempenho] SET FILESTREAM( NON_TRANSACTED_ACCESS = OFF ) 
+GO
+
+ALTER DATABASE [avaliacao_desempenho] SET TARGET_RECOVERY_TIME = 0 SECONDS 
+GO
+
+ALTER DATABASE [avaliacao_desempenho] SET DELAYED_DURABILITY = DISABLED 
+GO
+
+ALTER DATABASE [avaliacao_desempenho] SET  READ_WRITE 
+GO
+
+
---
+++ Documentação/3 - avaliacao_desempenho_data.sql
@@ -0,0 +1,66 @@
+USE [avaliacao_desempenho]
+GO
+
+/****************************************************************************************************
+* SituacaoCicloAvaliacao
+*****************************************************************************************************/
+
+INSERT INTO [dbo].[SituacaoCicloAvaliacao] ([Nome]) VALUES ('Criada')
+GO
+INSERT INTO [dbo].[SituacaoCicloAvaliacao] ([Nome]) VALUES ('Em definição de objetivos e metas')
+GO
+INSERT INTO [dbo].[SituacaoCicloAvaliacao] ([Nome]) VALUES ('Objetivos e metas definidos')
+GO
+INSERT INTO [dbo].[SituacaoCicloAvaliacao] ([Nome]) VALUES ('Em realização das auto-avaliações')
+GO
+INSERT INTO [dbo].[SituacaoCicloAvaliacao] ([Nome]) VALUES ('Em avaliação pelos gestores')
+GO
+INSERT INTO [dbo].[SituacaoCicloAvaliacao] ([Nome]) VALUES ('Em calibragem de rating e recomendação')
+GO
+INSERT INTO [dbo].[SituacaoCicloAvaliacao] ([Nome]) VALUES ('Encerrada')
+GO
+
+
+/****************************************************************************************************
+* StatusAvaliacaoColaborador
+*****************************************************************************************************/
+
+INSERT INTO [dbo].[StatusAvaliacaoColaborador] ([Nome]) VALUES ('Avaliação iniciada')
+GO
+
+INSERT INTO [dbo].[StatusAvaliacaoColaborador] ([Nome]) VALUES ('Definição de objetivos e metas')
+GO
+
+INSERT INTO [dbo].[StatusAvaliacaoColaborador] ([Nome]) VALUES ('Aprovação dos objetivos e metas definidos')
+GO
+
+INSERT INTO [dbo].[StatusAvaliacaoColaborador] ([Nome]) VALUES ('Objetivos e metas definidos')
+GO
+
+INSERT INTO [dbo].[StatusAvaliacaoColaborador] ([Nome]) VALUES ('Realização de auto-avaliação')
+GO
+
+
+/****************************************************************************************************
+* StatusPDI
+*****************************************************************************************************/
+
+INSERT INTO [dbo].[StatusPDI] ([Nome]) VALUES ('Criada')
+GO
+
+INSERT INTO [dbo].[StatusPDI] ([Nome]) VALUES ('Submetida à aprovação do gestor')
+GO
+
+INSERT INTO [dbo].[StatusPDI] ([Nome]) VALUES ('Aprovada')
+GO
+
+
+/****************************************************************************************************
+* Papel
+*****************************************************************************************************/
+
+INSERT INTO [dbo].[Papel] ([Nome]) VALUES ('Administrador')
+GO
+
+INSERT INTO [dbo].[Papel] ([Nome]) VALUES ('Colaborador')
+GO
