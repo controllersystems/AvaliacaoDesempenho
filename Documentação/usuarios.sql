ec787bb7ab21b54dd45a4a1ce5d706469155ffae
controllersystems <rodrigo.cruz@outlook.com>
2014-09-18 00:52:36 -0300

    Arquivos sql

---
+++ Documentação/usuarios.sql
@@ -0,0 +1,3 @@
+INSERT INTO [dbo].[Usuario] (Nome, Email, Papel_ID, GestorRubiID, UsuarioRubiID, CodigoEmpresaRubiUD, Login) VALUES ('ANDERSON LUIZ CARAMURU DA SILVA',	'caramuru@omnibrasil.com.br',1,	1490,492,1,	'caramuru')
+INSERT INTO [dbo].[Usuario] (Nome, Email, Papel_ID, GestorRubiID, UsuarioRubiID, CodigoEmpresaRubiUD, Login) VALUES ('FREDERICO FREITAS',	'frederico.freitas@omnibrasil.com.br',	1,	1490,	1120,	1,	'frederico.freitas')
+INSERT INTO [dbo].[Usuario] (Nome, Email, Papel_ID, GestorRubiID, UsuarioRubiID, CodigoEmpresaRubiUD, Login) VALUES ('RENATO CARVALHO',	'renato.carvalho@omnibrasil.com.br',	1,	1120,	1490,	1,	'renato.carvalho')
