e57e948f5a08eb2a180e4d9d82cf41b20fa1992d
controllersystems <rodrigo.cruz@outlook.com>
2014-10-15 23:00:17 -0300

    Add relatorios

---
+++ AvaliacaoDesempenho/Models/Relatorios/AcompanhamentoEtapaViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AvaliacaoDesempenho.Models.Relatorios
+{
+    public class AcompanhamentoEtapaViewModel
+    {
+        public int AnoReferencia { get; set; }
+        public int EtapaSelecionada { get; set; }
+        public List<ItemAcompanhamentoGeralViewModel> ListaAcompanhamentoGeral { get; set; }
+    }
+}
---
+++ AvaliacaoDesempenho/Models/Relatorios/ReuniaoCalibragemViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AvaliacaoDesempenho.Models.Relatorios
+{
+    public class ReuniaoCalibragemViewModel
+    {
+        public int AnoReferencia { get; set; }
+        public List<ItemReuniaoCalibragemViewModel> ListaAcompanhamentoGeral { get; set; }
+    }
+
+    public class ItemReuniaoCalibragemViewModel
+	{
+        public string Diretoria { get; set; }
+        public string Area { get; set; }
+        public string Gestor { get; set; }
+        public string NomeColaborador { get; set; }
+        public int Matricula { get; set; }
+        public string Cargo { get; set; }
+        public string Localidade { get; set; }
+        public DateTime DataAdmissao { get; set; }
+        public int TempoDeCasa { get; set; }
+        public string RecomendacaoDeRating { get; set; }
+        public string IndicacaoDePromocao { get; set; }
+        public string PerformanceGeral { get; set; }
+	} 
+}
